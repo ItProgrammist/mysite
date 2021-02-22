33efcad895b8f19c1b99d1c12aafea599b51b367
ItProgrammer <50521864+ItProgrammist@users.noreply.github.com>
2021-02-22 19:56:51 +0700

    Update views.py

--- pools/views.py
+++ pools/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
+def meme(meme):
+    return HttpResponse('<img src="https://millionstatusov.ru/pic/statpic/all8/5e04c21a52a39.jpg">')
