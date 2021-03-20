5682dee675ef8f1d9f417470c69eab75352a049e
ItProgrammist <kozlovdmitrij796@gmail.com>
2021-03-20 20:07:17 +0700

    Main page

--- polls/views.py
+++ polls/views.py
@@ -1,34 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-# Create your views here
-def index(request):
-    questions = Question.objects.all()
-    
-    context = {
-        "questions": questions
-    }
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "questions"
 
-    return render(request, "polls/index.html", context)
+    def get_queryset(self):
+        return Question.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 def meme(request):
     return HttpResponse('<img src="http://img.1001mem.ru/posts_temp/17-12-02/3922587.jpg">')
-# Страница /polls/q_id
-def detail(request, q_id):
-        #Берём ОДИН вопрос по PK, используя get()
-        question = Question.objects.get(pk=q_id)
-        context = {
-                "question": question,
-        }
-        return render(request, "polls/detail.html", context)
 
-
-def results(request, q_id):
-        res = 'Result for question number %s. ' % q_id
-        return HttpResponse(res)
 
 def vote(request, q_id):
-        res = 'Vote for question number %s. ' % q_id
-        return HttpResponse(res)
+        # Ответы пользователя - хранит pk
+        choices = request.POST.getlist("choice")
+        question = Question.objects.get(pk=q_id)
+        
+        res = ""
+        for c_pk in choices:
+                choice = question.choice_set.get(pk=c_pk)
+                choice.votes += 1
+                choice.save()
+                res += "<h1>%s</h1>" % question.choice_set.get(pk=c_pk).votes
+
+        return HttpResponseRedirect( reverse("polls:results", args = (q_id, )) )
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def results(request, q_id):
+    question = Question.objects.get(pk=q_id)
+    context = {
+        "question": question,
+    }
+
+    return render(request, "polls/results.html", context)
